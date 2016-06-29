fc36a3d9bc7501c79152a178ba8912806fb4f517
Hank Tom <hanktom@gmail.com>
2016-06-29 16:42:45 +0800

    AlertDialog.Builder

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,13 +1,47 @@
 package com.tom.bmi;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edWeight;
+    private EditText edHeight;
+    private Button bHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+
+    }
+
+    public void bmi(View v){
+        String w = edWeight.getText().toString();
+        String h = edHeight.getText().toString();
+        float weight = Float.parseFloat(w);
+        float height = Float.parseFloat(h);
+        float bmi = weight/(height*height);
+        Log.d("BMI", String.valueOf(bmi));
+        Toast.makeText(this, String.valueOf(bmi), Toast.LENGTH_LONG).show();
+        new AlertDialog.Builder(this)
+                .setMessage(bmi+"")
+                .setTitle("BMI運算")
+                .setPositiveButton("OK", null)
+                .show();
+    }
+
+    private void findViews() {
+        //取得畫面元件
+        edWeight = (EditText) findViewById(R.id.ed_weight);
+        edHeight = (EditText) findViewById(R.id.ed_height);
+        bHelp = (Button) findViewById(R.id.b_help);
     }
 }
